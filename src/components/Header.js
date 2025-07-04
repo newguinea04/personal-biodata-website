11a3a04f94fa19579a5f89acf634386a6bae97c6
Newguinea Pangalila <s22310098@student.unklab.ac.id>
2025-07-04 14:54:16 +0800

    Push ke Vercel

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="section bg-light text-center">
       <div className="container">
         <h1 className="section-title">
-          Selamat Datang di Website Biodata Saya
+          Selamat Datang di Website Newguinea Ratu Pangalila
         </h1>
         <p className="lead">Profil & Aktivitas Pribadi</p>
       </div>
